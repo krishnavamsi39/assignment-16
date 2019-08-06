82902ea13ff8a061bea145dd7ab56b362dacda3a
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-06 13:23:47 +0530

    [green state] for claculatorbutton component

--- src/Components/Calculator/CalculatorButtons/CalculatorButtons.test.js
+++ src/Components/Calculator/CalculatorButtons/CalculatorButtons.test.js
@@ -7,12 +7,22 @@ describe("Test for CalculatorButtons Component", () => {
   it("should check if add funtion is called", () => {
     const calculatorStore = new CalculatorStore();
     jest.spyOn(calculatorStore, "addCharacterToExpression");
-    const { getByText, debug } = render(
+    const { getByText } = render(
       <CalculatorButtons calculatorStore={calculatorStore} />
     );
 
     const add = getByText("+");
     fireEvent.click(add);
     expect(calculatorStore.addCharacterToExpression).toBeCalled();
+  });
+  it("should check if delete funtion is called", () => {
+    const calculatorStore = new CalculatorStore();
+    jest.spyOn(calculatorStore, "deleteCharacter");
+    const { getByText } = render(
+      <CalculatorButtons calculatorStore={calculatorStore} />
+    );
+    const deletechar = getByText("Del");
+    fireEvent.click(deletechar);
+    expect(calculatorStore.deleteCharacter).toBeCalled();
   });
 });
--- src/Components/Calculator/CalculatorButtons/index.js
+++ src/Components/Calculator/CalculatorButtons/index.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import BUTTONS from "../../../Constants/CalculatorButtons";
 import EachButton from "./EachButton";
 class CalculatorButtons extends Component {
-  handleClick = value => {};
+  handleClick = value => {
+    if (value === "Del") this.props.calculatorStore.deleteCharacter();
+    else this.props.calculatorStore.addCharacterToExpression(value);
+  };
 
   render() {
     return (
