448bf3a8936bc8f11f1060cdb400723a44b45540
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-06 10:20:07 +0530

    [refactored state]

--- src/Store/CalculatorStore/index.js
+++ src/Store/CalculatorStore/index.js
@@ -1,13 +1,10 @@
-import {observable,action} from "mobx"
-class CalculatorStore{
-    @observable expressionToBeEvaluated=""
+import { observable, action } from "mobx";
+class CalculatorStore {
+  @observable expressionToBeEvaluated = "";
 
-    @action
-    addCharacterToExpression(character){
-        this.expressionToBeEvaluated+=character
-
-    }
-
-
+  @action
+  addCharacterToExpression(character) {
+    this.expressionToBeEvaluated += character;
+  }
 }
 export default CalculatorStore;
