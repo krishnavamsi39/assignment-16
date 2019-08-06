98721a7b91f27e8b13d07e9d1f48ec6840465006
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-06 10:34:14 +0530

    [refactored state] for delete character function

--- src/Store/CalculatorStore/CalculatorStore.test.js
+++ src/Store/CalculatorStore/CalculatorStore.test.js
@@ -1,28 +1,27 @@
 import CalculatorStore from "./index";
 
 describe("tests for Calculator Store", () => {
-  it("should test add character function", () => {
-    const calculatorStore = new CalculatorStore();
+  let calculatorStore;
+  beforeEach(() => {
+    calculatorStore = new CalculatorStore();
     calculatorStore.addCharacterToExpression("3");
     calculatorStore.addCharacterToExpression("*");
     calculatorStore.addCharacterToExpression("5");
+  });
+  it("should test add character function", () => {
     expect(calculatorStore.expressionToBeEvaluated).toBe("3*5");
   });
   it("should clear the input ", () => {
-    const calculatorStore = new CalculatorStore();
-    calculatorStore.addCharacterToExpression("3");
-    calculatorStore.addCharacterToExpression("*");
-    calculatorStore.addCharacterToExpression("5");
     expect(calculatorStore.expressionToBeEvaluated).toBe("3*5");
     calculatorStore.clearTheInput();
     expect(calculatorStore.expressionToBeEvaluated).toBe("");
   });
   it("should delete a character", () => {
-    const calculatorStore = new CalculatorStore();
-    calculatorStore.addCharacterToExpression("3");
-    calculatorStore.addCharacterToExpression("*");
-    calculatorStore.addCharacterToExpression("5");
     calculatorStore.deleteCharacter();
     expect(calculatorStore.expressionToBeEvaluated).toBe("3*");
+    calculatorStore.deleteCharacter();
+    expect(calculatorStore.expressionToBeEvaluated).toBe("3");
+    calculatorStore.deleteCharacter();
+    expect(calculatorStore.expressionToBeEvaluated).toBe("");
   });
 });
