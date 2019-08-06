fef4aba36f91ac54845f3ee6a48a7f1da289be4e
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-06 10:28:18 +0530

    [red state] for delete character function

--- src/Store/CalculatorStore/CalculatorStore.test.js
+++ src/Store/CalculatorStore/CalculatorStore.test.js
@@ -17,4 +17,12 @@ describe("tests for Calculator Store", () => {
     calculatorStore.clearTheInput();
     expect(calculatorStore.expressionToBeEvaluated).toBe("");
   });
+  it("should delete a character", () => {
+    const calculatorStore = new CalculatorStore();
+    calculatorStore.addCharacterToExpression("3");
+    calculatorStore.addCharacterToExpression("*");
+    calculatorStore.addCharacterToExpression("5");
+    calculatorStore.deleteCharacter();
+    expect(calculatorStore.expressionToBeEvaluated).toBe("3*");
+  });
 });
--- src/Store/CalculatorStore/index.js
+++ src/Store/CalculatorStore/index.js
@@ -10,5 +10,7 @@ class CalculatorStore {
   clearTheInput() {
     this.expressionToBeEvaluated = "";
   }
+  @action
+  deleteCharacter() {}
 }
 export default CalculatorStore;
