bc069383a102852a8f3086e91c6234b70374281b
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-06 15:22:08 +0530

    [red state] for InputExpression component

---
+++ src/Components/Calculator/DisplayBar/InputExpression/InputExpression.test.js
@@ -0,0 +1,13 @@
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import React from "react";
+import CalculatorStore from "../../../../Store/CalculatorStore";
+import InputExpression from ".";
+describe("test for input expression component", () => {
+  it("should test the value of span tag", () => {
+    const calculatorStore = new CalculatorStore();
+    calculatorStore.addCharacterToExpression("*");
+    calculatorStore.addCharacterToExpression("-");
+    const { getByTestId } = render(<InputExpression />);
+    const input = getByTestId("expression");
+  });
+});
